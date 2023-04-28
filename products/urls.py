74e4580486ec2f8f0b610029ef60c71e859e780d
harkirat <harkriatsingh.tu@gmail.com>
2023-04-28 19:25:32 -0400

    step 4 add GET add APIs for products, categories,maker

---
+++ products/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from .views import ProductCategoryListView, MakerListView, ProductListView
+
+
+"""
+app_name must  be aded
+"""
+
+app_name = "products"
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="products-list"),
+    path("categories", ProductCategoryListView.as_view(), name="categories-list"),
+    path("makers", MakerListView.as_view(), name="makers-list"),
+]
